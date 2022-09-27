6966f17df154234fe99cdf55ec0beb0ea3acba54
Abdessattar Louhichi <louhichi.abdessattar@yahoo.com>
2022-09-27 15:23:26 +0100

    Créer un input de type texte

---
+++ Exercice2/exercice2.js
@@ -0,0 +1,7 @@
+
+document.writeln('<form  method="get">');
+
+document.writeln('<input type="text" name="name" id="name" size="100" value="Vous avez réussi votre exercice">'); 
+document.writeln('</form>')
+
+
