e70950cddcc8175063373b221686b787fcc26be2
Abdessattar Louhichi <louhichi.abdessattar@yahoo.com>
2022-09-27 17:58:14 +0100

    Ajouter script de validation d'un formulaire

---
+++ Exercice3/main.js
@@ -0,0 +1,6 @@
+var Name = document.getElementById('name')
+Name.setAttribute("required", "required")
+
+var Email = document.getElementById('email')
+Email.setAttribute("required", "required")
+console.log(Name)
