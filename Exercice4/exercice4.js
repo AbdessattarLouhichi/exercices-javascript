eb7c4348fee5d66101b5ffec3683e65d44f13970
Abdessattar Louhichi <louhichi.abdessattar@yahoo.com>
2022-09-27 22:29:25 +0100

    forcez le type de a et b à entier et exécuter à nouveau a+b

---
+++ Exercice4/exercice4.js
@@ -0,0 +1,17 @@
+let a = "1";
+let b = "2";
+let c = a+b;
+alert("La valeur de a est: " +a+ "\nLe type de a est: " +typeof(a)+
+     "\nLa valeur de b est: " +b+ "\nLe type de b est: " +typeof(b)+
+     "\nLa valeur de c est: " +c+ "\nLe type de c est: " +typeof(c)
+)
+
+
+a = Number(a);
+b = Number(b);
+c= a+b;
+
+alert("La valeur de a est: " +a+ "\nLe type de a est: " +typeof(a)+
+"\nLa valeur de b est: " +b+ "\nLe type de b est: " +typeof(b)+
+"\nLa valeur de c est: " +c+ "\nLe type de c est: " +typeof(c)
+)
