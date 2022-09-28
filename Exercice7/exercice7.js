457a4b47125df02c74e52016545946bede799a71
Abdessattar Louhichi <louhichi.abdessattar@yahoo.com>
2022-09-28 13:04:04 +0100

    Créer un instance Team d'ojet team

---
+++ Exercice7/exercice7.js
@@ -0,0 +1,33 @@
+
+function member(id,Name,grade) {
+    this.id = id;
+    this.Name = Name;
+    this.grade = grade;
+    this.toString = function () {
+        return 'id: ' + this.id+ ' Name: ' +this.Name+ ' grade: ' +this.grade ; 
+    }
+}
+ function team() {
+    this.members  = new Array();
+    this.add      = function(member) {
+      var index           = this.members.length;
+      this.members[index] = member;
+      return index;
+    }
+    this.toString = function() {
+      return this.members.join("\n");
+    }
+ }
+
+ 
+
+let Team = new team();
+
+Team.add(new member(1,"Ahmed","A"))
+Team.add(new member(2,"Ali","B"))
+Team.add(new member(3,"Yassin","C"))
+var string = Team.toString()
+var members = Team.members
+console.log(Team)
+console.log(string)
+console.log(members)
